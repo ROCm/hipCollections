c3f8504eef405eb7fd329236d5a935e042d77a3c
Philipp Samfass <philipp.samfass@amd.com>
2023-06-20 10:27:36 +0000

    fix: set warp size to 64 or HIP

--- include/cuco/static_multimap.cuh
+++ include/cuco/static_multimap.cuh
@@ -1390,7 +1390,7 @@ class static_multimap {
   /**
    * @brief Returns the warp size.
    */
-  static __host__ __device__ constexpr uint32_t warp_size() noexcept { return 32u; }
+  static __host__ __device__ constexpr uint32_t warp_size() noexcept { return 64u; } // TODO(HIP/AMD): adapt this to 32 or 64 if needed
 
   /**
    * @brief Custom deleter for unique pointer of slots.
