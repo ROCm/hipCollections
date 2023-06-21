49b595baa3d0257bc301912852b54b36f36529cd
Philipp Samfass <philipp.samfass@amd.com>
2023-06-21 16:40:54 +0200

    feat: added benchmark that tests different probing cg sizes for STATIC_MULTIMAP with hipBench

--- benchmarks/benchmark_defaults.hpp
+++ benchmarks/benchmark_defaults.hpp
@@ -14,23 +14,6 @@
  * limitations under the License.
  */
 
-// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 #pragma once
 
 #include <cuco/hash_functions.cuh>
@@ -51,8 +34,7 @@ using HASH_RANGE       = nvbench::type_list<cuco::identity_hash<char>,
 // cuco::murmurhash3_x86_128<char>,
 // cuco::murmurhash3_x64_128<char>>; // TODO handle tuple-like hash value
 
-// TODO(HIP/AMD): N needs to be changed to 100'000'000
-auto constexpr N             = 10'000'000;
+auto constexpr N             = 100'000'000;
 auto constexpr OCCUPANCY     = 0.5;
 auto constexpr MULTIPLICITY  = 1;
 auto constexpr MATCHING_RATE = 1.0;
---
+++ benchmarks/hash_table/static_multimap_cg/query_bench.cu.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2021-2023, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <defaults.hpp>
+#include <utils.hpp>
+
+#include <cuco/static_multimap.cuh>
+#include <cuco/utility/key_generator.hpp>
+
+#include <nvbench/nvbench.cuh>
+
+#include <thrust/device_vector.h>
+#include <thrust/transform.h>
+
+using namespace cuco::benchmark;
+using namespace cuco::utility;
+
+/**
+ * @brief A benchmark evaluating 'cuco::static_multimap::query' (`count` + `retrieve`) performance
+ */
+template <typename Key, typename Value, typename Dist, nvbench::int32_t CGSize>
+std::enable_if_t<(sizeof(Key) == sizeof(Value)), void> static_multimap_query(
+  nvbench::state& state, nvbench::type_list<Key, Value, Dist, nvbench::enum_type<CGSize>>)
+{
+  using pair_type = cuco::pair_type<Key, Value>;
+
+  auto const num_keys      = state.get_int64_or_default("NumInputs", defaults::N);
+  auto const occupancy     = state.get_float64_or_default("Occupancy", defaults::OCCUPANCY);
+  auto const matching_rate = state.get_float64_or_default("MatchingRate", defaults::MATCHING_RATE);
+
+  std::size_t const size = num_keys / occupancy;
+
+  thrust::device_vector<Key> keys(num_keys);
+
+  key_generator gen;
+  gen.generate(dist_from_state<Dist>(state), keys.begin(), keys.end());
+
+  thrust::device_vector<pair_type> pairs(num_keys);
+  thrust::transform(keys.begin(), keys.end(), pairs.begin(), [] __device__(Key const& key) {
+    return pair_type(key, {});
+  });
+
+  gen.dropout(keys.begin(), keys.end(), matching_rate);
+
+  state.add_element_count(num_keys);
+
+  cuco::static_multimap<
+    Key,
+    Value,
+    hip::thread_scope_device,
+    cuco::cuda_allocator<char>,
+    cuco::double_hashing<CGSize, cuco::murmurhash3_32<Key>, cuco::murmurhash3_32<Key>>>
+    map{size, cuco::empty_key<Key>{-1}, cuco::empty_value<Value>{-1}};
+  map.insert(pairs.begin(), pairs.end());
+  auto needed_output_space = map.count_outer(keys.begin(), keys.end());
+  pairs.resize(needed_output_space);
+
+  state.exec(nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
+    auto count = map.count_outer(keys.begin(), keys.end(), launch.get_stream());
+    map.retrieve_outer(keys.begin(), keys.end(), pairs.begin(), launch.get_stream());
+  });
+}
+
+template <typename Key, typename Value, typename Dist, nvbench::int32_t CGSize>
+std::enable_if_t<(sizeof(Key) != sizeof(Value)), void> static_multimap_query(
+  nvbench::state& state, nvbench::type_list<Key, Value, Dist, nvbench::enum_type<CGSize>>)
+{
+  state.skip("Key should be the same type as Value.");
+}
+
+NVBENCH_BENCH_TYPES(static_multimap_query,
+                    NVBENCH_TYPE_AXES(defaults::KEY_TYPE_RANGE,
+                                      defaults::VALUE_TYPE_RANGE,
+                                      nvbench::type_list<distribution::uniform>,
+                                      defaults::CG_TYPE_RANGE))
+  .set_name("static_multimap_query_uniform_occupancy")
+  .set_type_axes_names({"Key", "Value", "Distribution", "ProbingCGSize"})
+  .set_max_noise(defaults::MAX_NOISE)
+  .add_float64_axis("Occupancy", defaults::OCCUPANCY_RANGE);
+
+NVBENCH_BENCH_TYPES(static_multimap_query,
+                    NVBENCH_TYPE_AXES(defaults::KEY_TYPE_RANGE,
+                                      defaults::VALUE_TYPE_RANGE,
+                                      nvbench::type_list<distribution::uniform>,
+                                      defaults::CG_TYPE_RANGE))
+  .set_name("static_multimap_query_uniform_matching_rate")
+  .set_type_axes_names({"Key", "Value", "Distribution", "ProbingCGSize"})
+  .set_max_noise(defaults::MAX_NOISE)
+  .add_float64_axis("MatchingRate", defaults::MATCHING_RATE_RANGE);
+
+NVBENCH_BENCH_TYPES(static_multimap_query,
+                    NVBENCH_TYPE_AXES(defaults::KEY_TYPE_RANGE,
+                                      defaults::VALUE_TYPE_RANGE,
+                                      nvbench::type_list<distribution::uniform>,
+                                      defaults::CG_TYPE_RANGE))
+  .set_name("static_multimap_query_uniform_multiplicity")
+  .set_type_axes_names({"Key", "Value", "Distribution", "ProbingCGSize"})
+  .set_max_noise(defaults::MAX_NOISE)
+  .add_int64_axis("Multiplicity", defaults::MULTIPLICITY_RANGE);
