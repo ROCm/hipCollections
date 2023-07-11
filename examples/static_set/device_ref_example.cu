9ced8aad9e0e40f95f314cf7d24f9ccbbb55c018
Philipp Samfass <philipp.samfass@amd.com>
2023-07-11 18:43:14 +0200

    reflected namespace structure in folder structure

--- examples/static_set/device_ref_example.cu
+++ examples/static_set/device_ref_example.cu
@@ -57,7 +57,7 @@ template <typename SetRef, typename InputIterator>
 __global__ void custom_cooperative_insert(SetRef set, InputIterator keys, std::size_t n)
 {
   namespace cg =
-    hip_cooperative_groups_ext;  // todo (hip): change back to ROCm CG when API has been extended
+    hip_extensions::hip_cooperative_groups_ext;  // todo (hip): change back to ROCm CG when API has been extended
 
   constexpr auto cg_size = SetRef::cg_size;
 
