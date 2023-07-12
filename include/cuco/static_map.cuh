4634bd0a554e8b2011177fc7738bddd4968020c1
Mohammad Norouzi <114993067+monoatamd@users.noreply.github.com>
2023-07-12 10:57:08 +0200

    Remove unnecessary code

--- include/cuco/static_map.cuh
+++ include/cuco/static_map.cuh
@@ -1209,6 +1209,7 @@ namespace legacy {
  * individual threads.
  * @tparam Allocator Type of allocator used for device storage
  */
+
 #ifndef HIP_TILE_SIZE
 #define HIP_TILE_SIZE 4
 #endif
@@ -1216,28 +1217,6 @@ namespace legacy {
 #define HIP_BLOCK_SIZE 128
 #endif
 
-// #if HIP_STATIC_MAP_OPT == 0
-// #ifndef HIP_TILE_SIZE
-// #define HIP_TILE_SIZE 4
-// #endif
-// #ifndef HIP_BLOCK_SIZE
-// #define HIP_BLOCK_SIZE 128
-// #endif
-// #elif HIP_STATIC_MAP_OPT == 1  // optimize tile size
-// #ifndef HIP_TILE_SIZE
-// #define HIP_TILE_SIZE HIP_OPT_TILE_OR_BLOCK_SIZE
-// #endif
-// #ifndef HIP_BLOCK_SIZE
-// #define HIP_BLOCK_SIZE 128
-// #endif
-// #elif HIP_STATIC_MAP_OPT == 2  // optimize block size
-// #ifndef HIP_TILE_SIZE
-// #define HIP_TILE_SIZE 4
-// #endif
-// #ifndef HIP_BLOCK_SIZE
-// #define HIP_BLOCK_SIZE HIP_OPT_TILE_OR_BLOCK_SIZE
-// #endif
-// #endif
 template <typename Key,
           typename Value,
           cuda::thread_scope Scope = cuda::thread_scope_device,
