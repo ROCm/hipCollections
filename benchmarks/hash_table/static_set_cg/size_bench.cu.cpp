5208c147f7f18d6e200a92ee1c0264527e85bcae
Mohammad Norouzi <mnorouzi@amd.com>
2023-11-13 12:43:55 +0000

    enable static_set_cg_bench

--- benchmarks/hash_table/static_set_cg/size_bench.cu.cpp
+++ benchmarks/hash_table/static_set_cg/size_bench.cu.cpp
@@ -14,6 +14,23 @@
  * limitations under the License.
  */
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #include <defaults.hpp>
 #include <utils.hpp>
 
@@ -60,7 +77,7 @@ void static_set_size(nvbench::state& state,
   set.insert(keys.begin(), keys.end());
 
   state.exec(nvbench::exec_tag::sync,
-             [&](nvbench::launch& launch) { auto const size = set.size(launch.get_stream()); });
+             [&](nvbench::launch& launch) { auto const size = set.size({launch.get_stream()}); });
 }
 
 NVBENCH_BENCH_TYPES(static_set_size,
