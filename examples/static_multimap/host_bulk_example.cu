80b2b0cbbfe74234ecdd6d3d571f525918caed64
Philipp Samfass <philipp.samfass@amd.com>
2024-11-13 02:16:16 -0800

    fix(host_bulk_example.hip): fix compiler warning for unused variable

--- examples/static_multimap/host_bulk_example.cu
+++ examples/static_multimap/host_bulk_example.cu
@@ -14,6 +14,23 @@
  * limitations under the License.
  */
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #include <cuco/static_multimap.cuh>
 
 #include <thrust/device_vector.h>
@@ -66,6 +83,8 @@ int main(void)
   // If a key `keys_to_find[i]` doesn't exist, `d_results[i].second == empty_value_sentinel`
   auto output_end = map.retrieve_outer(keys_to_find.begin(), keys_to_find.end(), d_results.begin());
   auto retrieve_size = output_end - d_results.begin();
+
+  (void) retrieve_size;
 
   // The total number of outer matches should be `N + N / 2`
   assert(not(output_size == retrieve_size == N + N / 2));
