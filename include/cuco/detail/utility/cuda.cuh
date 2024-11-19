aa806328a68369282a4131d8f02525ac1ffadad2
Philipp Samfass <psamfass@amd.com>
2024-11-19 15:33:23 +0100

    fix(cuda.cuh): fix compiler warning about missing inline keyword

--- include/cuco/detail/utility/cuda.cuh
+++ include/cuco/detail/utility/cuda.cuh
@@ -13,6 +13,23 @@
  * See the License for the specific language governing permissions and
  */
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #pragma once
 
 #include <cuco/detail/utility/cuda.hpp>
@@ -48,7 +65,7 @@ __device__ constexpr int32_t warp_size() noexcept { return 32; }
  *
  * @return The global thread index
  */
-__device__ static index_type global_thread_id() noexcept
+__device__ static inline index_type global_thread_id() noexcept
 {
   return index_type{threadIdx.x} + index_type{blockDim.x} * index_type{blockIdx.x};
 }
@@ -58,7 +75,7 @@ __device__ static index_type global_thread_id() noexcept
  *
  * @return The grid stride
  */
-__device__ static index_type grid_stride() noexcept
+__device__ static inline index_type grid_stride() noexcept
 {
   return index_type{gridDim.x} * index_type{blockDim.x};
 }
