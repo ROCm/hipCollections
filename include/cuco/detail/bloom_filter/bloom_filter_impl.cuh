5cf27b5bddd03f519ea57ca4d4fca28a205eb91b
Daniel Jünger <djuenger@nvidia.com>
2025-02-27 03:06:58 +0100

    Fix function visibility in bloom_filter_impl (#684)
    (cherry picked from commit ac18c647e28dc72f05c2bf44a22cedae8a867a50)

--- include/cuco/detail/bloom_filter/bloom_filter_impl.cuh
+++ include/cuco/detail/bloom_filter/bloom_filter_impl.cuh
@@ -76,7 +76,6 @@ class bloom_filter_impl {
   static constexpr auto thread_scope    = Scope;
   static constexpr auto words_per_block = policy_type::words_per_block;
 
- private:
   __host__ __device__ static constexpr size_t max_vec_bytes() noexcept
   {
     constexpr auto word_bytes  = sizeof(word_type);
@@ -85,7 +84,6 @@ class bloom_filter_impl {
                           block_bytes);  // aiming for 2xLDG128 -> 1 sector per thread
   }
 
- public:
   struct alignas(max_vec_bytes()) filter_block_type {
    private:
     word_type data_[words_per_block];
