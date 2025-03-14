2e35118a822adfb8e38b37214f9812ebdc4f2fc1
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-03-14 07:16:04 -0700

    replaced __trap by assert and __throw_runtime_error

--- include/cuco/detail/bloom_filter/default_filter_policy_impl.cuh
+++ include/cuco/detail/bloom_filter/default_filter_policy_impl.cuh
@@ -14,6 +14,23 @@
  * limitations under the License.
  */
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #pragma once
 
 #include <cuco/detail/error.hpp>
@@ -74,7 +91,9 @@ class default_filter_policy_impl {
       NV_IS_DEVICE,
       (if (pattern_bits_ > max_pattern_bits_from_hash or pattern_bits_ < min_pattern_bits or
            pattern_bits_ > max_pattern_bits) {
-        __trap();  // TODO this kills the kernel and corrupts the CUDA context. Not ideal.
+          assert(false);
+          __throw_runtime_error;
+          //__trap();  // TODO this kills the kernel and corrupts the CUDA context. Not ideal.
       }))
   }
 
