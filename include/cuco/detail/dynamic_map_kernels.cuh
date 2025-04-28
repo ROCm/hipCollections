c7f69369b753bfadc5e02a0a3817d777b277a5cd
Mingjie LIU <79076959+caaatch22@users.noreply.github.com>
2025-04-28 10:22:14 +0800

    Use relaxed memory-order in dynamic_map::find (#702)
    (cherry picked from commit 5e8885d2d3fb24060f26f5e4986ccc4509368bfa)

--- include/cuco/detail/dynamic_map_kernels.cuh
+++ include/cuco/detail/dynamic_map_kernels.cuh
@@ -416,7 +416,7 @@ CUCO_KERNEL void find(InputIt first,
       auto submap_view = submap_views[i];
       auto found       = submap_view.find(key, hash, key_equal);
       if (found != submap_view.end()) {
-        found_value = found->second;
+        found_value = found->second.load(cuda::std::memory_order_relaxed);
         break;
       }
     }
@@ -493,7 +493,7 @@ CUCO_KERNEL void find(InputIt first,
       auto submap_view = submap_views[i];
       auto found       = submap_view.find(tile, key, hash, key_equal);
       if (found != submap_view.end()) {
-        found_value = found->second;
+        found_value = found->second.load(cuda::std::memory_order_relaxed);
         break;
       }
     }
