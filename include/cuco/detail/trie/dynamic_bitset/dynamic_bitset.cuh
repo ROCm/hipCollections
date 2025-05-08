564e446a763ebb716241717c66ac5842a25c54ff
Michael Obersteiner <Michael.Obersteiner@amd.com>
2025-05-08 07:34:45 -0700

    fix: added typename keyword to get rid of compiler errors

--- include/cuco/detail/trie/dynamic_bitset/dynamic_bitset.cuh
+++ include/cuco/detail/trie/dynamic_bitset/dynamic_bitset.cuh
@@ -15,6 +15,23 @@
  * limitations under the License.
  */
 
+// Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #pragma once
 
 #include <cuda/std/array>
@@ -85,7 +102,7 @@ class dynamic_bitset {
   using size_type = std::size_t;  ///< size type to specify bit index
   using word_type = uint64_t;     ///< word type
   /// Type of the allocator to (de)allocate words
-  using allocator_type = typename std::allocator_traits<Allocator>::rebind_alloc<word_type>;
+  using allocator_type = typename std::allocator_traits<Allocator>::template rebind_alloc<word_type>;
 
   /// Number of bits per block. Note this is a tradeoff between space efficiency and perf.
   static constexpr size_type words_per_block = 4;
@@ -326,9 +343,9 @@ class dynamic_bitset {
 
  private:
   /// Type of the allocator to (de)allocate ranks
-  using rank_allocator_type = typename std::allocator_traits<Allocator>::rebind_alloc<rank_type>;
+  using rank_allocator_type = typename std::allocator_traits<Allocator>::template rebind_alloc<rank_type>;
   /// Type of the allocator to (de)allocate indices
-  using size_allocator_type = typename std::allocator_traits<Allocator>::rebind_alloc<size_type>;
+  using size_allocator_type = typename std::allocator_traits<Allocator>::template rebind_alloc<size_type>;
 
   allocator_type allocator_;  ///< Words allocator
   size_type n_bits_;          ///< Number of bits dynamic_bitset currently holds
