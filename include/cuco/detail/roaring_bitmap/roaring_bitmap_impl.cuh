5ee115f94dc123f07e8e60163b023f3bcc296e23
Philipp Samfass <philipp.samfass@amd.com>
2025-11-17 07:37:17 -0500

    Add conditional hipCUB support for roaring_bitmap
    - Use hipcub/hipcub.hpp on __HIP_PLATFORM_AMD__
    - Use cub/device/device_transform.cuh on CUDA platforms
    - Add namespace alias: namespace cub = hipcub
    - Ensures compatibility with HIP

--- include/cuco/detail/roaring_bitmap/roaring_bitmap_impl.cuh
+++ include/cuco/detail/roaring_bitmap/roaring_bitmap_impl.cuh
@@ -14,14 +14,42 @@
  * limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include <cuco/detail/error.hpp>
 #include <cuco/detail/roaring_bitmap/roaring_bitmap_storage.cuh>
 #include <cuco/detail/roaring_bitmap/util.cuh>
+#include <cuda/functional>
 #include <cuco/utility/traits.hpp>
-
+#ifdef __HIP_PLATFORM_AMD__
+#include <hipcub/hipcub.hpp>
+namespace cub = hipcub;
+#else
 #include <cub/device/device_transform.cuh>
+#endif
+
 #include <cuda/std/cstddef>
 #include <cuda/std/cstdint>
 #include <cuda/std/functional>
